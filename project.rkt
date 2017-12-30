968dc50426d7a91a30bc47c2d33cd3139f161fd6
Iman Tabrizian <tabrizian@outlook.com>
2017-12-30 23:22:32 +0330

    Add solution to problem one

--- project.rkt
+++ project.rkt
@@ -24,68 +24,76 @@
 
 ;; Problem 1
 
-(define (racketlist->numexlist xs) "CHANGE")
-(define (numexlist->racketlist xs) "CHANGE")
+(struct apair (head tail) #:transparent)
+(define (racketlist->numexlist xs)
+  (cond [(null? xs) (munit)]
+        [else       (apair (car xs) (racketlist->numexlist (cdr xs)))]))
+
+(define (numexlist->racketlist xs)
+  (cond [(equal? xs munit) null]
+        [else       (cons (apair-head xs) (numexlist->racketlist (apair-tail xs)))]))
+
+
 
 ;; Problem 2
 
 ;; lookup a variable in an environment
 ;; Complete this function
-(define (envlookup env str)
-  (cond [(null? env) (error "unbound variable during evaluation" str)]
-  		"CHANGE" 
-		)
+;(define (envlookup env str)
+;  (cond [(null? env) (error "unbound variable during evaluation" str)]
+;  		"CHANGE" 
+;		))
 
 ;; Do NOT change the two cases given to you.  
 ;; DO add more cases for other kinds of NUMEX expressions.
 ;; We will test eval-under-env by calling it directly even though
 ;; "in real life" it would be a helper function of eval-exp.
-(define (eval-under-env e env)
-  (cond [(var? e) 
-         (envlookup env (var-string e))]
-        [(add? e) 
-         (let ([v1 (eval-under-env (add-e1 e) env)]
-               [v2 (eval-under-env (add-e2 e) env)])
-           (if (and (int? v1)
-                    (int? v2))
-               (int (+ (int-num v1) 
-                       (int-num v2)))
-               (error "NUMEX addition applied to non-number")))]
+;(define (eval-under-env e env)
+  ;(cond [(var? e) 
+   ;      (envlookup env (var-string e))]
+    ;    [(add? e) 
+     ;    (let ([v1 (eval-under-env (add-e1 e) env)]
+      ;         [v2 (eval-under-env (add-e2 e) env)])
+       ;    (if (and (int? v1)
+        ;            (int? v2))
+         ;      (int (+ (int-num v1) 
+          ;             (int-num v2)))
+           ;    (error "NUMEX addition applied to non-number")))]
         ;; CHANGE add more cases here
-        [#t (error (format "bad NUMEX expression: ~v" e))]))
+     ;   [#t (error (format "bad NUMEX expression: ~v" e))]))
 
 ;; Do NOT change
-(define (eval-exp e)
-  (eval-under-env e null))
+;(define (eval-exp e)
+ ; (eval-under-env e null))
         
 ;; Problem 3
 
-(define (ifmunit e1 e2 e3) "CHANGE")
+;(define (ifmunit e1 e2 e3) "CHANGE")
 
-(define (mlet* bs e2) "CHANGE")
+;(define (mlet* bs e2) "CHANGE")
 
-(define (ifeq e1 e2 e3 e4) "CHANGE")
+;(define (ifeq e1 e2 e3 e4) "CHANGE")
 
 ;; Problem 4
 
-(define numex-map "CHANGE")
+;(define numex-map "CHANGE")
 
-(define numex-mapAddN
-  (mlet "map" numex-map
-        "CHANGE (notice map is now in NUMEX scope)"))
+;(define numex-mapAddN
+ ; (mlet "map" numex-map
+  ;      "CHANGE (notice map is now in NUMEX scope)"))
 
 ;; Challenge Problem
 
-(struct fun-challenge (nameopt formal body freevars) #:transparent) ;; a recursive(?) 1-argument function
+;(struct fun-challenge (nameopt formal body freevars) #:transparent) ;; a recursive(?) 1-argument function
 
 ;; We will test this function directly, so it must do
 ;; as described in the assignment
-(define (compute-free-vars e) "CHANGE")
+;(define (compute-free-vars e) "CHANGE")
 
 ;; Do NOT share code with eval-under-env because that will make grading
 ;; more difficult, so copy most of your interpreter here and make minor changes
-(define (eval-under-env-c e env) "CHANGE")
+;(define (eval-under-env-c e env) "CHANGE")
 
 ;; Do NOT change this
-(define (eval-exp-c e)
-  (eval-under-env-c (compute-free-vars e) null))
+;(define (eval-exp-c e)
+ ; (eval-under-env-c (compute-free-vars e) null))
